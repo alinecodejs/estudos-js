3c78759f868606e570bfce6b28a0353fb3d59155
Aline S <alinecode.js@gmail.com>
2021-09-28 22:50:40 -0300

    Create scripts.js

---
+++ semaforo/scripts.js
@@ -0,0 +1,16 @@
+const img = document.getElementById('img'); 
+const buttons = document.getElementById('buttons');
+
+const traffic = (event) => {
+  turnOn[event.target.id]();
+
+};
+
+const turnOn = {
+  'red':    () => img.src = './img/vermelho.png',
+  'yellow': () => img.src = './img/amarelo.png',
+  'green':  () => img.src = './img/verde.png'
+}
+
+
+buttons.addEventListener('click', traffic);
